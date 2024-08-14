b3fa8d563b98860b4222cfd64c137a37768108f4
cshyeon32 <139736071+cshyeon32@users.noreply.github.com>
2024-08-14 09:50:44 +0900

    feat :: HomeView

---
+++ apple-foundation-program-3th/View/ToDoListView.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoListView.swift
+//  apple-foundation-program-3th
+//
+//  Created by dgsw8th07 on 8/14/24.
+//
+
+import SwiftUI
+
+struct ToDoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoListView()
+}
