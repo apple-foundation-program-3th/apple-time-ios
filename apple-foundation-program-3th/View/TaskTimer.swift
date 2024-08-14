14af1e1026d8efc9e0a82412650054cd0a44630e
miraexhoi <miraechoi@dgsw.hs.kr>
2024-08-14 09:44:40 +0900

    feat :: task timer & apple time model

---
+++ apple-foundation-program-3th/View/TaskTimer.swift
@@ -0,0 +1,18 @@
+//
+//  TaskTimer.swift
+//  apple-foundation-program-3th
+//
+//  Created by Mirae on 8/14/24.
+//
+
+import SwiftUI
+
+struct TaskTimer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskTimer()
+}
---
+++ apple-foundation-program-3th/ViewModel/AppleTimeModel.swift
@@ -0,0 +1,18 @@
+//
+//  AppleTimeModel.swift
+//  apple-foundation-program-3th
+//
+//  Created by Mirae on 8/14/24.
+//
+
+import SwiftUI
+
+struct AppleTimeModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppleTimeModel()
+}
